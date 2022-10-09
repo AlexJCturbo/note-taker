c340d76e6faa0d9904d4b439af9a0110e129f04e
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-09 01:48:32 -0500

    initial server file created

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3001;
+
 const { notes } = require('./db/db.json')
 
-app.get('/notes', (req, res) => {
-  res.send('Hello!');
+
+function findByTitle(title, notesArray) {
+  const result = notesArray.filter(note => note.title === title)[0];
+  return result;
+}
+
+app.get('/api/notes', (req, res) => {
+  let results = notes;
+  console.log(req.query)
+  res.json(results);
 });
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+
+app.get('/api/notes/:title', (req, res) => {
+  const result = findByTitle(req.params.title, notes);
+    res.json(result);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
