50365b9d79ee89e7861111f5e8d03a39d9975ba8
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-09 22:58:22 -0500

    created POST route to write data in json file

--- server.js
+++ server.js
@@ -14,25 +14,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-
-
-
+//Funtion to find notes
 function findByTitle(title, notesArray) {
   const result = notesArray.filter(note => note.title === title)[0];
   return result;
 }
 
-function createNewAnimal(body, notesArray) {
+//Funtion to create new notes
+function createNote(body, notesArray) {
   const note = body;
   notesArray.push(note);
   fs.writeFileSync(
     path.join(__dirname, './db/db.json'),
     JSON.stringify({db: notesArray}, null, 2)
   );
-
   return note;
 }
 
+//Funtion to validate date when creating new note
+function validateNewNote(note) {
+  if (!note.title || typeof note.title !== 'string') {
+    return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+    return false;
+  }
+  return true;
+}
 
 //GET routes
 app.get('/api/notes', (req, res) => {
@@ -48,20 +56,22 @@ app.get('/api/notes/:title', (req, res) => {
   }
 });
 
-
 //POST routes
 app.post('/api/notes', (req, res) => {
   //re.body is where the user inputs the content
+
+  //req.body.id = (db.length+1).toString(); -> Option to create ID
+  
   //Using uniqid to generate a the notes ID based on the time and the process
-  //req.body.id = (db.length+1).toString();
   req.body.id = (uniqid.process('id-')).toString();
-  //const note = createNote(req.body, db)
 
-  const note = createNewAnimal(req.body, db);
-
-  res.json(note);
+  if (!validateNewNote(req.body)) {
+    res.status(400).send('Please introduce a name and a text for your note.');
+  } else {
+    const note = createNote(req.body, db);
+    res.json(note);
+  }
 });
-
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
