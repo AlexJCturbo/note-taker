d3d5894e258f9a6908bbb9e25af045f1c5eab9ba
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-09 22:47:08 -0500

    created POST route to write data in json file

--- server.js
+++ server.js
@@ -1,19 +1,65 @@
 const express = require('express');
-
-const PORT = process.env.PORT || 3001;
-
 const app = express();
-
-const notes = require("./db/db.json");
-
 const fs = require("fs");
 const path = require("path");
+const uniqid = require('uniqid'); 
+
+const PORT = process.env.PORT || 3010;
+const { db } = require("./db/db.json");
+
+// app.use method allows us to mount function to the server known as middleware
+// takes incoming POST data and parse the string or array into key/value pairs
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data into the req.body object
+app.use(express.json());
 
 
 
 
+
+function findByTitle(title, notesArray) {
+  const result = notesArray.filter(note => note.title === title)[0];
+  return result;
+}
+
+function createNewAnimal(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({db: notesArray}, null, 2)
+  );
+
+  return note;
+}
+
+
+//GET routes
 app.get('/api/notes', (req, res) => {
-  res.json(notes);
+  res.json(db);
+});
+
+app.get('/api/notes/:title', (req, res) => {
+  const result = findByTitle(req.params.title, db);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404)
+  }
+});
+
+
+//POST routes
+app.post('/api/notes', (req, res) => {
+  //re.body is where the user inputs the content
+  //Using uniqid to generate a the notes ID based on the time and the process
+  //req.body.id = (db.length+1).toString();
+  req.body.id = (uniqid.process('id-')).toString();
+  //const note = createNote(req.body, db)
+
+  const note = createNewAnimal(req.body, db);
+
+  res.json(note);
 });
 
 
